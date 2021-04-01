aea9d1688054cb63b963efcd76f81050236a83e0
Santosh.Bhat.Contractor <santosh.bhat.contractor@3dscom>
2021-04-01 18:12:57 +0530

    Kafka Basic Pub Sub Implementation

---
+++ KafkaProducer_CA/KafkaProducer_CA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+
+namespace KafkaProducer_CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string payload = Console.ReadLine(); //"Welcome to kafka!"; 
+                string topic = "IDGTestTopic";
+                Message msg = new Message(payload);
+                Uri uri = new Uri("http://localhost:9092");
+                var options = new KafkaOptions(uri);
+                var router = new BrokerRouter(options);
+                var client = new Producer(router);
+                client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
